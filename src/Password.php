c9028698026c4bab9548039dd267cc4dffc9a7de
Džuris <juris@glaive.pro>
2020-05-05 00:07:16 +0300

    Remove unused method

--- src/Password.php
+++ src/Password.php
@@ -7,20 +7,6 @@ use Hash;
 
 class Password implements CastsInboundAttributes
 {
-	/**
-     * Cast the given value.
-     *
-     * @param  \Illuminate\Database\Eloquent\Model  $model
-     * @param  string  $key
-     * @param  mixed  $value
-     * @param  array  $attributes
-     * @return mixed
-     */
-    public function get($model, string $key, $value, array $attributes)
-	{
-		return $value;
-	}
-
     /**
      * Prepare the given value for storage.
      *
